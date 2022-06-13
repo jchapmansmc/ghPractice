a9a9ba8bd1bf8425303474ed0381c5b581ae9f82
Jack Chapman <Mrjchapman1993@gmail.com>
2022-06-13 06:34:47 +0100

    Add project files.

---
+++ ghPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ghPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Edit me");
+            //add a new line here
+        }
+    }
+}
