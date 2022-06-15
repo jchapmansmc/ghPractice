8b47688f3ac75ca6acb0efcf0f1e05736c18fb66
Blazej Stolarczuk <blazejstolarczuki@wp.pl>
2022-06-15 11:50:28 +0100

    Editing line 13

--- ghPractice/Program.cs
+++ ghPractice/Program.cs
@@ -10,7 +10,7 @@ namespace ghPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Edit me");
+            Console.Write("Ezz is a tad silly");
             //add a new line here
             //new line here
             //newer line here
