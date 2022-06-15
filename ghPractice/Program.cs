c97f2fd91f685d31a84e43ed66986403e9ec52f8
Azzam Mohamed <10MohamedA@student.smchull.org>
2022-06-15 11:50:37 +0100

    ...

--- ghPractice/Program.cs
+++ ghPractice/Program.cs
@@ -10,7 +10,7 @@ namespace ghPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Edit me");
+            Console.WriteLine("hello");
             //add a new line here
             //new line here
             //newer line here
