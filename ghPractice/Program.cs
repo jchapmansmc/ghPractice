efc205a78971b0847c41f147b27be8705066958c
Ezzulddin Al-Qaisi <10Al-QaisiE@student.smchull.org>
2022-06-15 11:52:14 +0100

    .

--- ghPractice/Program.cs
+++ ghPractice/Program.cs
@@ -10,7 +10,7 @@ namespace ghPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Edit me");
+            Console.WriteLine("Ezz was here");
             //add a new line here
             //new line here
             //newer line here
